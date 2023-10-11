1d780cebcea5755645ba742301772676e1174bf4
utsavbansal <ubansal1996@gmail.com>
2023-10-11 17:22:49 +0530

    Initial project with Spring Initializr with DB
    Signed-off-by: utsavbansal <ubansal1996@gmail.com>

---
+++ src/main/java/com/example/taskman/TaskmanWithdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanWithdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanWithdbApplication.class, args);
+	}
+
+}
